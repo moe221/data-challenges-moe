e64b2df0d3fa7ad5d1a0d013d13d96d92985dbcd
Mohamed Abuhalala <myf.abuhalala@gmail.com>
2021-12-21 18:39:33 +0100

    practicing partitions

--- 01-Python/04-SQL-Advanced/03-Rank-And-Partition/queries.py
+++ 01-Python/04-SQL-Advanced/03-Rank-And-Partition/queries.py
@@ -1,8 +1,35 @@
 # pylint:disable=C0111,C0103
 
 def order_rank_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = """
 
+    SELECT OrderID, CustomerID, OrderDate, RANK() OVER(
+        PARTITION BY CustomerID
+        ORDER BY OrderDate
+        ) AS OrderRank
+    FROM Orders
+
+    """
+    db.execute(query)
+    return db.fetchall()
 
 def order_cumulative_amount_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = """
+
+    SELECT Orders.OrderID,
+    Orders.CustomerID,
+    (Orders.OrderDate),
+    SUM(SUM(UnitPrice * Quantity)) OVER(
+        PARTITION BY CustomerID
+        ORDER BY orders.OrderDate
+        ) AS OrderCumulativeAmount
+    FROM Orders
+    JOIN OrderDetails ON
+    OrderDetails.OrderID = Orders.OrderID
+    GROUP BY Orders.OrderID
+    ORDER BY Orders.CustomerID;
+
+
+    """
+    db.execute(query)
+    return db.fetchall()
