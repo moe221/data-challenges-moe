aa28e8d63f67cebf1aa8e59fe9ae2a7c3c747066
Mohamed Abuhalala <myf.abuhalala@gmail.com>
2021-12-21 10:04:42 +0100

    warm up query

--- 01-Python/04-SQL-Advanced/00-Warmup/school.py
+++ 01-Python/04-SQL-Advanced/00-Warmup/school.py
@@ -1,15 +1,28 @@
 # pylint:disable=C0111,C0103
 
+import sqlite3
+
+
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
+
+    query = f"""
+
+    SELECT * FROM students
+    WHERE students.birth_city = ?
+
+    """
+
+    db.execute(query, (city,))
+    return (db.fetchall())
 
 
 # To test your code, you can **run it** before running `make`
 #   => Uncomment the following lines + run:
 #   $ python school.py
-#
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
-# print(students_from_city(db, 'Paris'))
+
+if __name__ == "__main__":
+
+    conn = sqlite3.connect('data/school.sqlite')
+    db = conn.cursor()
+    print(students_from_city(db, 'Paris'))
