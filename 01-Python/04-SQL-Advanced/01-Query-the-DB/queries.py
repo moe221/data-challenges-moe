6cfa67925869f471768e4bd1a077e83e53e631ca
Mohamed Abuhalala <myf.abuhalala@gmail.com>
2021-12-21 10:29:51 +0100

    finished challenge 2

--- 01-Python/04-SQL-Advanced/01-Query-the-DB/queries.py
+++ 01-Python/04-SQL-Advanced/01-Query-the-DB/queries.py
@@ -2,14 +2,47 @@
 
 def query_orders(db):
     # return a list of orders displaying each column
-    pass  # YOUR CODE HERE
+    query = """
+
+    SELECT * FROM Orders
+    ORDER BY OrderID ASC
+
+    """
+    db.execute(query)
+    return db.fetchall()
 
 def get_orders_range(db, date_from, date_to):
     # return a list of orders displaying all columns with OrderDate between date_from and date_to
-    pass  # YOUR CODE HERE
+    query = """
+
+    SELECT * FROM Orders
+    WHERE OrderDate > ? AND OrderDate <= ?
+    ORDER BY OrderDate ASC;
+
+    """
+    db.execute(query, (date_from, date_to))
+    return db.fetchall()
+
 
 def get_waiting_time(db):
     # get a list with all the orders displaying each column
     # and calculate an extra TimeDelta column displaying the number of days
     # between OrderDate and ShippedDate, ordered by ascending TimeDelta
-    pass  # YOUR CODE HERE
+    query = """
+
+    SELECT *, julianday(ShippedDate) - julianday(OrderDate)
+    as delivery_time FROM Orders
+    ORDER BY delivery_time ASC;
+
+    """
+    db.execute(query)
+    return db.fetchall()
+
+
+# if __name__ == "__main__":
+
+#     import sqlite3
+#     conn = sqlite3.connect('data/ecommerce.sqlite')
+#     db = conn.cursor()
+
+#     print(get_waiting_time(db))
