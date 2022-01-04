b6ae858f2fdd33e6617c77e4df0a782a5cebd589
Mohamed Abuhalala <myf.abuhalala@gmail.com>
2022-01-04 10:50:09 +0200

    environment variables practice

--- 01-Python/01-Programming-Basics/06-Environment/flask_option.py
+++ 01-Python/01-Programming-Basics/06-Environment/flask_option.py
@@ -4,7 +4,13 @@ import os
 
 def start():
     """returns the right message"""
-    pass  # YOUR CODE HERE
+    FLASK_ENV = os.getenv("FLASK_ENV")
+
+    if FLASK_ENV == "development":
+        return "Starting in development mode..."
+
+    elif FLASK_ENV == "production" or FLASK_ENV is None:
+        return "Starting in production mode..."
 
 if __name__ == "__main__":
     print(start())
