90235d30232fe2920adc37ca0672e9656e3f4c68
Mohamed Abuhalala <myf.abuhalala@gmail.com>
2022-01-04 10:32:52 +0200

    warmup

--- 01-Python/01-Programming-Basics/05-System-Parameters/args.py
+++ 01-Python/01-Programming-Basics/05-System-Parameters/args.py
@@ -1,2 +1,6 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring
 
+import sys
+
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
--- 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
+++ 01-Python/01-Programming-Basics/05-System-Parameters/calc.py
@@ -1,12 +1,20 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring,eval-used
 import sys
+import operator
 
+OPS = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv,  # use operator.div for Python 2
+    '%': operator.mod,
+    '^': operator.xor,
+}
 
 def main():
     """Implement the calculator"""
-    pass  # YOUR CODE HERE
+    return OPS[sys.argv[2]](int(sys.argv[1]), int(sys.argv[3]))
 
 
 if __name__ == "__main__":
     print(main())
-    
