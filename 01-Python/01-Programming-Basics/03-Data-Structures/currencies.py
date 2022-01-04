dc8f23f2a08166670bcb00f3314b9e52ebe20883
Mohamed Abuhalala <myf.abuhalala@gmail.com>
2022-01-04 11:54:28 +0200

    data types refresher

--- 01-Python/01-Programming-Basics/03-Data-Structures/currencies.py
+++ 01-Python/01-Programming-Basics/03-Data-Structures/currencies.py
@@ -1,11 +1,15 @@
 # pylint: disable=missing-docstring
 
-# TODO: add some currency rates
-RATES = {}
+dict_keys = ["USDEUR", "GBPEUR", "CHFEUR", "EURGBP"]
+ditc_values = [0.85, 1.13, 0.86, 0.885]
+RATES = dict(zip(dict_keys, ditc_values))
 
 def convert(amount, currency):
     """returns the converted amount in the given currency
     amount is a tuple like (100, "EUR")
     currency is a string
     """
-    pass  # YOUR CODE HERE
+    rate = RATES.get(f"{amount[1]}{currency}", None)
+    if rate is None:
+        return None
+    return round(rate * amount[0])
